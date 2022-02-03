adb585cd15b9b8de80cc7eec49961a9e4f7f7386
divya9072 <divi9072@gmail.com>
2022-02-03 19:21:21 +0530

    first commit

---
+++ src/components/Announcement/Announcement.jsx
@@ -0,0 +1,7 @@
+import "./Announcement.css";
+
+const Announcement = () => {
+    return <div className='ContainerDeal'>Super Deal! Free Shipping on Orders Over $50</div>;
+  };
+  
+export default Announcement;
