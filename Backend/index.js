f274801503d34c3f5314711c3e55500846e71125
divya9072 <divi9072@gmail.com>
2022-02-07 19:11:46 +0530

    Backend completed

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoute=require("./routes/user")
+const authRoute=require("./routes/auth")
+const productRoute=require("./routes/product")
+const cartRoute=require("./routes/cart")
+const orderRoute=require("./routes/order")
 // to use env file
 const dotenv=require("dotenv");  
 // set up our express app
@@ -14,9 +18,13 @@ mongoose.connect(process.env.MONGO_URL).then(()=>console.log('DB Connected'))
     console.log(err);
 })
 
-app.use("/api/users",userRoute);
-
 app.use(express.json());
+
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
+app.use("/api/products",productRoute);
+app.use("/api/carts",cartRoute);
+app.use("/api/orders",orderRoute);
 
 // listen for requests
 app.listen(process.env.PORT || 5000, function(){
